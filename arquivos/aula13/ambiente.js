8b5f92f0f8641347eb174081b7bbd6bb568b0a55
Gabriel <gabriel.moura2009@hotmial.com>
2020-07-14 20:02:13 -0300

    aula 14

---
+++ arquivos/aula13/ambiente.js
@@ -0,0 +1,13 @@
+var contador = 0
+//do while
+do{
+    console.log(`Passo do while ${contador}`)
+    contador++
+}while (contador < 6)
+
+//while
+while(contador < 7 ) {
+    console.log(`Passo while ${contador}`)
+}
+
+
