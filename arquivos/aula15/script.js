10bad347218867bbb5bcb60ac5b37074c091205b
Gabriel <gabriel.moura2009@hotmial.com>
2020-07-18 12:13:33 -0300

    aula15

---
+++ arquivos/aula15/script.js
@@ -0,0 +1,15 @@
+let num = [5, 8, 2, 9, 3]
+
+console.log(`${num.indexOf(9)}`)// vai retorna a posição 3
+
+for (var i = 0; i < num.length; i++){
+    console.log(num[i])
+    
+}
+console.log(`fim`)
+// ou
+for(let y in num2){
+    console.log(`posição ${y} tem o valor ${num2[y]}`)
+}
+
+
