b075930f065f93181f10344224f5b7cb780e9956
Gabriel <gabriel.moura2009@hotmial.com>
2020-07-20 20:15:17 -0300

    ex18 finish

--- arquivos/exercicios/ex18/scripts/script.js
+++ arquivos/exercicios/ex18/scripts/script.js
@@ -1,28 +1,51 @@
 var valorDig = document.querySelector('input#inNum')
 var resulNum = document.querySelector('select#resultado')
+var escreve = document.querySelector('div#result')
 var guardaValores = []
 
 function adicionar(){
+    let numero = Number(valorDig.value)
     if(valorDig.value == 0 || valorDig.value > 100){
-        window.alert(`Valor inválido ou já encontrado na Lista`)
+        return msgErro()
         
-    }else{
-        let numero = Number(valorDig.value)
+    }else if (guardaValores.indexOf(numero) == -1){
         guardaValores.push(numero)
-        
+
         let item = document.createElement('option')
         item.text = `Valor ${numero} adicionado`
         resulNum.appendChild(item)
-
+        
+    }else{
+        return msgErro()
     }
 }
 
 function verificar(){
-    for(var i = 0; i < guardaValores.length; i++){
-        console.log(`item ${i} na posicao ${guardaValores[i]}`)
+    if(guardaValores == 0){
+       return msgErro()
+
+    }else{
+        let tamVetor = guardaValores.length
+        let min = Math.min(...guardaValores)
+        let max = Math.max(...guardaValores)
+        let soma = 0
+        
+        for(var i = 0; i < guardaValores.length; i++){
+            soma += Number(guardaValores[i])    
+            
+        }
+
+        let media = (soma / tamVetor).toFixed(2)
+    
+        escreve.innerHTML = `Ao todo temos ${guardaValores.length} armazenados <br>
+        O menor valor informado foi ${min} <br>
+        O maior valor informado foi ${max} <br>
+        A soma dos valores é ${soma} <br>
+        A média dos valores é ${media} <br>`
+
     }
-    let escreve = document.querySelector('div#result')
-    //escreve.innerHTML += `valores digitado é ${numero} <br> `
 }
 
-//|| guardaValores.indexOf(numero)
+function msgErro(){
+    window.alert(`Valor inválido ou o valor já se encontra no array`)
+}
